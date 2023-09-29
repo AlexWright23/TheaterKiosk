3436a6f75418fc619480369f9507f2c08e077bd4
AlexWright23 <honeybadger937@gmail.com>
2023-09-29 14:00:51 -0400

    Added pseudo code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+//Class age
+//        main()
+//        num = age
+//        output "Please enter your age "
+//        input age
+//        if age >= 21 then
+//        output "Here is your wrist band"
+//        return
+//        endClass
 public class Main {
     public static void main(String[] args)
     {
