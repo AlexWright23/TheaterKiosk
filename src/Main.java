ba7dbe6352461d6e822e90b3e0d6764398859ffb
AlexWright23 <honeybadger937@gmail.com>
2023-09-29 13:47:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 21) {
+            System.out.println("Congratulations! You get a wristband!");
+        }
+        scanner.close();
+    }
+}
